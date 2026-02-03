e9086ea51f09a90896b2f5a1937d198e366f1912
aand1242 <nathkorn.wai@gmail.com>
2026-02-04 00:29:22 +0700

    start frame of my game and setting the background

--- Main.java
+++ Main.java
@@ -1,4 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame screen = new JFrame();
+        screen.setSize(640,360);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try{
+            File backgroundpath = new File("C:\\Users\\natha\\Documents\\project\\source_pic\\bordtest.jpg");
+            BufferedImage background =  ImageIO.read(backgroundpath);
+            ImageIcon icon = new ImageIcon(background);
+            JLabel label = new JLabel(icon);
+            screen.add(label);
+        }catch(Exception e){
+            System.out.println("i cant see");
+        }
+        
+        
+        screen.setVisible(true);
     }
 }
